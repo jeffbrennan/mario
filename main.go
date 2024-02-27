e8d4068a1d541d0ad80db553b71f5ea7e06b2f50
jeffbrennan <jeffbrennan10@gmail.com>
2024-02-27 01:14:48 -0500

    add auth to main

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	mario "github.com/jeffbrennan/mario/cmd"
-	"github.com/jeffbrennan/mario/cmd/mario"
+	_ "github.com/jeffbrennan/mario/cmd/auth"
 	_ "github.com/jeffbrennan/mario/cmd/summarize"
 )
 
